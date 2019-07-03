143eba856c7a13d4c0c324ccef2db610aa288a82
Pabls <pabls@yandex.ru>
2019-07-03 22:15:47 +0300

    many fix

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,51 +1,13 @@
 package ru.skillbranch.devintensive.utils
 
+import java.lang.StringBuilder
+
 object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?>{
+        val parts = fullName?.split(" ")
+        val firstName = parts?.getOrNull(0).orEmpty().trim().ifEmpty { null }
+        val lastName = parts?.getOrNull(1).orEmpty().trim().ifEmpty { null }
 
-    private val chars = mapOf(
-        "а" to "a",
-        "б" to "b",
-        "в" to "v",
-        "г" to "g",
-        "д" to "d",
-        "е" to "e",
-        "ё" to "e",
-        "ж" to "zh",
-        "з" to "z",
-        "и" to "i",
-        "й" to "i",
-        "к" to "k",
-        "л" to "l",
-        "м" to "m",
-        "н" to "n",
-        "о" to "o",
-        "п" to "p",
-        "р" to "r",
-        "с" to "s",
-        "т" to "t",
-        "у" to "u",
-        "ф" to "f",
-        "х" to "h",
-        "ц" to "c",
-        "ч" to "ch",
-        "ш" to "sh",
-        "щ" to "sh'",
-        "ъ" to "",
-        "ы" to "i",
-        "ь" to "",
-        "э" to "e",
-        "ю" to "yu",
-        "я" to "ya"
-    )
-
-    fun parseFullName(fullName: String?): Pair<String?, String?> {
-        var parts: List<String>? = fullName?.trim()?.split(" ")
-        if (parts != null) {
-            if(parts.size > 2)
-                parts = parts.filter { !it.isNullOrEmpty() }
-        }
-        val firstName = if (parts?.get(0).isNullOrEmpty()) null else parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
         return firstName to lastName
     }
 
@@ -94,7 +56,7 @@ object Utils {
         map['х'] = "h"
         map['ц'] = "c"
         map['ч'] = "ch"
-        map['ш'] = "sh'"
+        map['ш'] = "sh"
         map['щ'] = "sh'"
         map['ъ'] = ""
         map['ы'] = "i"
@@ -106,88 +68,11 @@ object Utils {
         return map
     }
 
-//    fun transliteration(payload: String, divider: String = " "): String? {
-//        val (firstName, lastName) = parseFullName(payload)
-//
-//        var engFirstName = ""
-//        var engLastName = ""
-//
-//        firstName?.forEach { char ->
-//            var c = chars[char.toLowerCase().toString()]
-//
-//            if (c != null) {
-//                engFirstName += if (char.isUpperCase()) {
-//                    c[0].toUpperCase()
-//                } else {
-//                    c[0]
-//                }
-//
-//                if (c.length > 1) {
-//                    c.forEachIndexed { index, char ->
-//                        if (index > 0) {
-//                            engFirstName += char
-//                        }
-//                    }
-//                }
-//
-//            }
-//        }
-//
-//        lastName?.forEach { char ->
-//            var c = chars[char.toLowerCase().toString()]
-//
-//            if (c != null) {
-//                engLastName += if (char.isUpperCase()) {
-//                    c[0].toUpperCase()
-//                } else {
-//                    c[0]
-//                }
-//
-//                if (c.length > 1) {
-//                    c.forEachIndexed { index, char ->
-//                        if (index > 0) {
-//                            engLastName += char
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        var res = ""
-//
-//        if (engFirstName.isNullOrEmpty() && firstName != null) {
-//            engFirstName = firstName
-//        }
-//        if (engLastName.isNullOrEmpty() && lastName != null) {
-//            engLastName = lastName
-//        }
-//
-//        return if (!engLastName.isNullOrEmpty()) return engFirstName + divider + engLastName else return engFirstName
-//    }
-
     fun toInitials(firstName: String?, lastName: String?): String? {
-        var res: String? = null
-        val firstNameInital = getFirstCharToUpperCase(firstName)
-        val lastNameInital = getFirstCharToUpperCase(lastName)
-        if (firstNameInital != null) {
-            res = firstNameInital.toString()
-        }
-        if (lastNameInital != null) {
-            if (res != null) {
-                res += lastNameInital
-            } else {
-                res = lastNameInital.toString()
-            }
-        }
-        return res
-    }
-
-    private fun getFirstCharToUpperCase(value: String?): Char? {
-        var result = value?.trim()
-        return if (result.isNullOrEmpty()) {
-            null
-        } else {
-            result[0]?.toUpperCase()
-        }
+        val name = firstName.orEmpty().trim().getOrNull(0)?.toUpperCase()
+        val surname = lastName.orEmpty().trim().getOrNull(0)?.toUpperCase()
+        val firstInit = name?.toString() ?: ""
+        val secondInit = surname?.toString() ?: ""
+        return "$firstInit$secondInit".ifEmpty { null }
     }
 }
