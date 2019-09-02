8abd87db93d58fb22bbfb89df5b156e2014e01b9
Pavel Semin <psemin@golama.ru>
2019-09-02 18:43:11 +0300

    fix

--- app/src/main/java/ru/skillbranch/devintensive/models/data/Chat.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/data/Chat.kt
@@ -3,20 +3,21 @@ package ru.skillbranch.devintensive.models.data
 import androidx.annotation.VisibleForTesting
 import ru.skillbranch.devintensive.extensions.shortFormat
 import ru.skillbranch.devintensive.models.BaseMessage
+import ru.skillbranch.devintensive.models.ImageMessage
 import ru.skillbranch.devintensive.models.TextMessage
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
 data class Chat(
-    val id: String,
-    val title: String,
-    val members: List<User> = listOf(),
-    var messages: MutableList<BaseMessage> = mutableListOf(),
-    var isArchived: Boolean = false
+        val id: String,
+        val title: String,
+        val members: List<User> = listOf(),
+        var messages: MutableList<BaseMessage> = mutableListOf(),
+        var isArchived: Boolean = false
 ) {
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     fun unreadableMessageCount(): Int {
-        return messages.size
+        return messages.filter { !it.isReaded }.size
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
@@ -26,12 +27,11 @@ data class Chat(
 
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     fun lastMessageShort(): Pair<String?, String?> =
-        when (val lastMessage = messages.lastOrNull()) {
-            is TextMessage -> lastMessage.text to "${lastMessage.from.firstName}"
-//            is TextMessage -> lastMessage.text to "${lastMessage.from.firstName} ${lastMessage.from.lastName ?: ""}"
-//            is TextMessage -> lastMessage.text to "${Utils.transliteration("${lastMessage.from.firstName ?: ""} ${lastMessage.from.lastName ?: ""}")}"
-            else -> "Сообщений еще нет" to ""
-        }
+            when (val lastMessage = messages.lastOrNull()) {
+                is TextMessage -> lastMessage.text to "${lastMessage.from.firstName}"
+                is ImageMessage -> "${lastMessage.from.firstName} - отправил фото" to "${lastMessage.from.firstName}"
+                else -> "Сообщений еще нет" to ""
+            }
 
     private fun isSingle(): Boolean = members.size == 1
 
@@ -39,38 +39,34 @@ data class Chat(
         val user = members.first()
         return if (isSingle()) {
             ChatItem(
-                id,
-                user.avatar,
-                Utils.toInitials(user.firstName, user.lastName) ?: "??",
-                "${user.firstName ?: ""} ${user.lastName ?: ""}",
-                lastMessageShort().first,
-                unreadableMessageCount(),
-                lastMessageDate()?.shortFormat(),
-                user.isOnline
+                    id,
+                    user.avatar,
+                    Utils.toInitials(user.firstName, user.lastName) ?: "??",
+                    "${user.firstName ?: ""} ${user.lastName ?: ""}",
+                    lastMessageShort().first,
+                    unreadableMessageCount(),
+                    lastMessageDate()?.shortFormat(),
+                    user.isOnline
             )
         } else {
             ChatItem(
-                id,
-                null,
-                Utils.toInitials(user.firstName, null) ?: "??",
-                title,
-                lastMessageShort().first,
-                unreadableMessageCount(),
-                lastMessageDate()?.shortFormat(),
-                false,
-                ChatType.GROUP,
-                lastMessageShort().second
+                    id,
+                    null,
+                    Utils.toInitials(user.firstName, null) ?: "??",
+                    title,
+                    lastMessageShort().first,
+                    unreadableMessageCount(),
+                    lastMessageDate()?.shortFormat(),
+                    false,
+                    ChatType.GROUP,
+                    lastMessageShort().second
             )
         }
     }
 }
-
 
 enum class ChatType {
     SINGLE,
     GROUP,
     ARCHIVE
 }
-
-
-
